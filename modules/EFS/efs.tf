31e38ed24953246050e4baccfd4016c2dfa10ea4
Ubuntu <ubuntu@ip-172-31-89-147.ec2.internal>
2023-04-19 18:42:02 +0000

    trraform

--- modules/EFS/efs.tf
+++ modules/EFS/efs.tf
@@ -40,15 +40,15 @@ resource "aws_efs_file_system" "ACS-efs" {
 # set first mount target for the EFS 
 resource "aws_efs_mount_target" "subnet-1" {
   file_system_id  = aws_efs_file_system.ACS-efs.id
-  subnet_id       = aws_subnet.private[2].id
-  security_groups = [aws_security_group.datalayer-sg.id]
+  subnet_id       = var.efs-subnet-1
+  security_groups = var.efs-sg
 }
 
 # set second mount target for the EFS 
 resource "aws_efs_mount_target" "subnet-2" {
   file_system_id  = aws_efs_file_system.ACS-efs.id
-  subnet_id       = aws_subnet.private[3].id
-  security_groups = [aws_security_group.datalayer-sg.id]
+  subnet_id       = var.efs-subnet-2
+  security_groups = var.efs-sg
 }
 
 # create access point for wordpress
